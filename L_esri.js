037cad4a774ac9de1fa28ad5b8aca8f03444d920
BIG-BEN001 <benjaminochieng99@gmail.com>
2022-06-28 15:55:43 +0300

    commit2

--- L_esri.js
+++ L_esri.js
@@ -81,8 +81,6 @@ $("#popr").on('click', function() {
 });
 
 
-
-
 countyPops.on('click', function(e) {
     selectedCounty = e.layer.feature.properties;
     displaySelected();
@@ -542,3 +540,27 @@ function setParent(el, newParent) {
     newParent.appendChild(el);
 }
 setParent(htmlObject, a);
+
+
+// create a fullscreen button and add it to the map
+L.control.fullscreen({
+    position: 'topleft', // change the position of the button can be topleft, topright, bottomright or bottomleft, default topleft
+    title: 'View Map in Full Screen Mode', // change the title of the button, default Full Screen
+    titleCancel: 'Exit full Screen Mode', // change the title of the button when fullscreen is on, default Exit Full Screen
+    content: null, // change the content of the button, can be HTML, default null
+    forceSeparateButton: true, // force separate button to detach from zoom buttons, default false
+    forcePseudoFullscreen: true, // force use of pseudo full screen even if full screen API is available, default false
+    fullscreenElement: false // Dom element to render in full screen, false by default, fallback to map._container
+}).addTo(map);
+
+// events are fired when entering or exiting fullscreen.
+map.on('enterFullscreen', function() {
+    console.log('Entered Full Screen');
+});
+
+map.on('exitFullscreen', function() {
+    console.log('Exited Full Screen');
+});
+
+// you can also toggle fullscreen from map object
+// map.toggleFullscreen();
